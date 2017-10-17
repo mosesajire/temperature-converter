657d3d0033723c6fa4a5721dea620ff7747cec3b
Moses Ajireloja <ajiremoses@gmail.com>
2017-10-17 10:45:43 +0100

    Temperature Converter

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Temperature Converter</title>      
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keyword" content="PHP, 'Temperature Converter', 'Github Project', ConsonanceNg">
+        <meta name="description" content="This PHP program converts temperature from celcius to fahrenheit, and vice versa.">
+        <meta name="author" content="Moses Ajireloja">
+        <link rel="stylesheet" href="style.css" type="text/css" media="all">
+    </head>  
